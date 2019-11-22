8b2a0aa3ba2355ae6ecaf66132c9347f5c2e1bf3
Alexandru <alexandruzavoianu@gmail.com>
2019-11-22 11:22:30 +0200

    initial push

---
+++ server.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Alexandru-PC on 21-Nov-2019.
+ */
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').parse()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
